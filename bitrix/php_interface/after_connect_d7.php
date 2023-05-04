dcd041bb7def81b8d84dd3267711e51d4351708e
root <root@ibs-training.ru>
2023-05-04 20:11:02 +0300

    update

--- bitrix/php_interface/after_connect_d7.php
+++ bitrix/php_interface/after_connect_d7.php
@@ -1,5 +1,6 @@
 <?php
-/* Ansible managed: /etc/ansible/roles/web/templates/after_connect_d7.php.j2 modified on 2015-06-15 12:29:59 by root on 185.26.112.96-vds.nichost.ru */
+/* Ansible managed */
+$connection = \Bitrix\Main\Application::getConnection();
 
-$this->queryExecute("SET NAMES 'cp1251'");
-
+$connection->queryExecute("SET NAMES 'utf8'");
+$connection->queryExecute("SET collation_connection = 'utf8_unicode_ci'");
--- bitrix/templates/learning/description.php
+++ bitrix/templates/learning/description.php
@@ -1,8 +1,10 @@
 <?
-$arTemplate = array (
-  'NAME' => 'Прохождение курса обучения 9.1.0',
-  'DESCRIPTION' => 'Шаблон модуля обучения',
-  'SORT' => 400,
-  'TYPE' => '',
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+\Bitrix\Main\Localization\Loc::loadLanguageFile(__FILE__);
+
+$arTemplate = Array(
+	"NAME" => \Bitrix\Main\Localization\Loc::getMessage("LEARNING_TEMPLATE_DESCRIPTION_NAME"),
+	"DESCRIPTION" => \Bitrix\Main\Localization\Loc::getMessage("LEARNING_TEMPLATE_DESCRIPTION_DESC")
 );
 ?>
