61acabf4c4c840718c7a3dd3294e8a730295075e
Alex <onlinely@yandex.ru>
2024-12-18 15:54:44 +0300

    изменение шаблона формы курсов

--- local/templates/ibs-training_new/components/bitrix/form.result.new/sign.course/script.js
+++ local/templates/ibs-training_new/components/bitrix/form.result.new/sign.course/script.js
@@ -85,3 +85,9 @@ class SignCourse
 document.addEventListener("DOMContentLoaded", () => {
     new SignCourse();
 });
+
+/* Yandex.Metrika counter */ (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); /* /Yandex.Metrika counter */
+
+ym(23056159, 'getClientID', function(clientID) {
+    document.getElementById('clientID').value = clientID;
+});
