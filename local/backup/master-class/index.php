a6174f45ca885d938a8b575ff368034b98b0899f
IBS-Traning-Center <education@ibs.ru>
2025-02-19 18:34:30 +0700

    удаление Google Analytics

--- local/backup/master-class/index.php
+++ local/backup/master-class/index.php
@@ -17,16 +17,6 @@ include($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.ph
 <!--[if lt IE 9]>
 <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 <![endif]-->
-<script type="text/javascript">
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-9384348-1', 'auto');
-	  ga('require', 'displayfeatures');
-	  ga('send', 'pageview');
-</script>
 <?php
 $elementID = $APPLICATION->IncludeComponent(
     "bitrix:news.detail",
