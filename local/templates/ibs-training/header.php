df7b12a797301cf73585ece9bb9ab7c728c308be
IBS-Traning-Center <education@ibs.ru>
2025-02-19 18:25:06 +0700

    удаление Google Tag Manager

--- local/templates/ibs-training/header.php
+++ local/templates/ibs-training/header.php
@@ -100,21 +100,6 @@ $request = $application->getContext()->getRequest();
     <script data-skip-moving="true" src="/static/libs/modernizr.js"></script>
     <script data-skip-moving="true" src="/static/libs/jquery.js"></script>
     <meta name="yandex-verification" content="5d503f9f5bb0b3f6" />
-    <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-N8N8SHJ');</script>
-    <!-- End Google Tag Manager -->
 
     <script type="text/javascript">
         (function (i, s, o, g, r, a, m) {
@@ -138,12 +123,6 @@ $request = $application->getContext()->getRequest();
 </head>
 <body>
 <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(23056159, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, ecommerce:"dataLayer" }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/23056159" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8N8SHJ" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
 <?php
 if ($USER->IsAdmin()) {
     $APPLICATION->ShowPanel();
