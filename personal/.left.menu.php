d6cb4a9acee3e33cf52671ab726f719b2048806c
Kirill <kocherov@addamant.ru>
2025-12-18 22:46:20 +0300

    135556| Личный кабинет / Временная деактивация разделов блока ЛК.

--- personal/.left.menu.php
+++ personal/.left.menu.php
@@ -1,32 +1,75 @@
-<?
+<?php
 $aMenuLinks = Array(
-	Array(
-		"Мои настройки", 
-		"profile/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Мои заказы", 
-		"order/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Моя корзина", 
-		"cart/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Подписка", 
-		"subscribe/", 
-		Array(), 
-		Array(), 
-		"" 
-	)
+//    Array(
+//        "Главная",
+//        "/personal/",
+//        Array(),
+//        Array(
+//            "ICON" => "home"
+//        ),
+//        ""
+//    ),
+//    Array(
+//        "Бонусные баллы",
+//        "/personal/bonus/",
+//        Array(),
+//        Array(
+//            "ICON" => "bonus"
+//        ),
+//        ""
+//    ),
+    Array(
+        "Мои документы",
+        "/personal/docs/",
+        Array(),
+        Array(
+            "ICON" => "documents"
+        ),
+        ""
+    ),
+//    Array(
+//        "Мои заказы",
+//        "/personal/order/",
+//        Array(),
+//        Array(
+//            "ICON" => "orders"
+//        ),
+//        ""
+//    ),
+//    Array(
+//        "Уведомления",
+//        "/personal/notifications/",
+//        Array(),
+//        Array(
+//            "ICON" => "notifications"
+//        ),
+//        ""
+//    ),
+    Array(
+        "Настройки",
+        "/personal/profile/",
+        Array(),
+        Array(
+            "ICON" => "settings"
+        ),
+        ""
+    )
+    // Array(
+    //     "Моя корзина",
+    //     "/cart/",
+    //     Array(),
+    //     Array(
+    //         "ICON" => "cart"
+    //     ),
+    //     ""
+    // ),
+    // Array(
+    //     "Подписка",
+    //     "/subscribe/",
+    //     Array(),
+    //     Array(
+    //         "ICON" => "subscribe"
+    //     ),
+    //     ""
+    // )
 );
-?>
