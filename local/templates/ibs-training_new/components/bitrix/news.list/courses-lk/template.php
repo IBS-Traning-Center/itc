44459e8800be49f0f5b6a5c436060e296a1cb78c
Kirill <kocherov@addamant.ru>
2025-12-21 15:01:01 +0300

    135560| Личный кабинет / Добавление пункта меню и верстка функционала «Мои документы» и подключение его к разработанному функционалу в административной панели. Сертификаты, Курсы, Программы.

---
+++ local/templates/ibs-training_new/components/bitrix/news.list/courses-lk/template.php
@@ -0,0 +1,98 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+use Local\Util\Functions;
+$this->setFrameMode(true);
+?>
+<section class="lk-section">
+    <h2 class="lk-section__title">Курсы</h2>
+
+    <div class="lk-cards">
+        <div class="lk-card course">
+            <div class="lk-card__body">
+                <div class="lk-card__meta">
+                    <span class="lk-card__code">DEV-001_NET</span>
+                    <span class="lk-badge">Team Lead</span>
+                    <span class="lk-card__duration">24 ч</span>
+                </div>
+
+                <div class="lk-card__meta-secondary">
+                    <div class="lk-card__date-secondary">
+                        <?= Functions::buildSVG(
+                            'date',
+                            'local/templates/ibs-training_new/components/bitrix/news.list/courses-lk/ico'
+                        ) ?>
+                        <span class="lk-card__date-text-secondary"> 14 июля 2025 (через 5 дней)</span>
+                    </div>
+                    <div class="lk-card__time-secondary">
+
+                        <span class="lk-card__time-text-secondary">  <?= Functions::buildSVG(
+                                'time',
+                                'local/templates/ibs-training_new/components/bitrix/news.list/courses-lk/ico'
+                            ) ?>14:00–18:00</span>
+                    </div>
+                </div>
+
+                <div class="lk-card__title">
+                    Шаблоны проектирования GoF. Редакция для .NET
+                </div>
+
+                <div class="lk-card__subtitle">
+                    Тренер — Иванов Евгений
+                </div>
+
+                <div class="lk-card__price">39 900 ₽</div>
+
+                <a href="#" class="lk-card__btn">Скачать документ</a>
+            </div>
+        </div>
+
+        <div class="lk-card course">
+            <div class="lk-card__body">
+                <div class="lk-card__meta">
+                    <span class="lk-card__code">DEV-001_NET</span>
+                    <span class="lk-badge">Team Lead</span>
+                    <span class="lk-card__duration">24 ч</span>
+                </div>
+
+                <div class="lk-card__meta-secondary">
+                    <div class="lk-card__date-secondary">
+                        <?= Functions::buildSVG(
+                            'date',
+                            'local/templates/ibs-training_new/components/bitrix/news.list/courses-lk/ico'
+                        ) ?>
+                        <span class="lk-card__date-text-secondary"> 14 июля 2025 (через 5 дней)</span>
+                    </div>
+                    <div class="lk-card__time-secondary">
+
+                        <span class="lk-card__time-text-secondary">  <?= Functions::buildSVG(
+                                'time',
+                                'local/templates/ibs-training_new/components/bitrix/news.list/courses-lk/ico'
+                            ) ?>14:00–18:00</span>
+                    </div>
+                </div>
+
+                <div class="lk-card__title">
+                    Шаблоны проектирования GoF. Редакция для .NET
+                </div>
+
+                <div class="lk-card__subtitle">
+                    Тренер — Иванов Евгений
+                </div>
+
+                <div class="lk-card__price">39 900 ₽</div>
+
+                <a href="#" class="lk-card__btn">Скачать документ</a>
+            </div>
+        </div>
+    </div>
+</section>
