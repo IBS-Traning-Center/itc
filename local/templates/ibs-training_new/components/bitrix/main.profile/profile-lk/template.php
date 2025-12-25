c426023ddf08455f9ed899aef737ea785f83c37a
Kirill <kocherov@addamant.ru>
2025-12-26 01:18:33 +0300

    136130 | ЛК/Настройки/Профиль/ Отсутствует маска для ввода номера телефона и блокировка ввода букв

--- local/templates/ibs-training_new/components/bitrix/main.profile/profile-lk/template.php
+++ local/templates/ibs-training_new/components/bitrix/main.profile/profile-lk/template.php
@@ -1,14 +1,12 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 ?>
-<?
+<?php
 $client_id = 'VLEAK7BKIEFJ9FAEU9M3QQ5BTN2JUH4PT3CQM86VU2NOIHDHH5LAQ6UNJKI375IM';
-$address = 'https://hh.ru/oauth/authorize?response_type=code&role=applicant&client_id=&client_id=' . $client_id;
+$address = 'https://hh.ru/oauth/authorize?response_type=code&role=applicant&client_id=' . $client_id;
 ?>
-</head>
-<body>
 
-    <div class="container-lk lk-profile-wrapper">
+<div class="container-lk lk-profile-wrapper">
     <main class="main-content">
         <div class="tabs">
             <a class="tab active">Профиль</a>
@@ -40,8 +38,16 @@ $address = 'https://hh.ru/oauth/authorize?response_type=code&role=applicant&clie
 
             <div class="field-blue">
                 <label>Телефон (не обязательно)</label>
-                <input type="text" name="PERSONAL_PHONE" value="<?=htmlspecialcharsbx($arResult["arUser"]["PERSONAL_PHONE"])?>" />
+                <input
+                        type="text"
+                        name="PERSONAL_PHONE"
+                        id="phone-input"
+                        placeholder="+7 (___) ___-__-__"
+                        value="<?=htmlspecialcharsbx($arResult["arUser"]["PERSONAL_PHONE"])?>"
+                        autocomplete="off"
+                />
             </div>
+
             <div class="field-blue">
                 <label>Эл. почта</label>
                 <input type="text" value="<?=htmlspecialcharsbx($arResult["arUser"]["EMAIL"])?>" disabled />
@@ -59,12 +65,11 @@ $address = 'https://hh.ru/oauth/authorize?response_type=code&role=applicant&clie
             <div class="field-gray">
                 <input type="text" name="WORK_POSITION" placeholder="Должность" value="<?=htmlspecialcharsbx($arResult["arUser"]["WORK_POSITION"])?>" />
             </div>
+
             <div class="field-gray">
                 <input type="text" name="UF_TELEGRAM" placeholder="@TelegramNickName" value="<?=htmlspecialcharsbx($arResult["arUser"]["UF_TELEGRAM"])?>" />
             </div>
 
-
-            <!-- Привязка hh.ru -->
             <div class="hh-section">
                 <div class="hh-link">
                     <div class="hh-logo">hh</div>
@@ -84,11 +89,47 @@ $address = 'https://hh.ru/oauth/authorize?response_type=code&role=applicant&clie
                 <div class="delete-icon"></div>
                 <div>Удалить аккаунт</div>
             </div>
-
         </form>
-
     </main>
 </div>
 
-</body>
-</html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/5.0.9/inputmask.min.js"
+        integrity="sha512-1h6rN0X4o5yC365tF4v5n14nM6UgR2p+1eNSKAfI7wzJ6cN8oV1x4uWnz+Hg9zC6MxtW+KmO2b2GLEP9wtXg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const phoneInput = document.getElementById('phone-input');
+
+        const mask = new Inputmask({
+            mask: "+7 (999) 999-99-99",
+            placeholder: "_",
+            showMaskOnHover: false,
+            clearIncomplete: false,
+            onBeforePaste: function (pastedValue) {
+                return pastedValue.replace(/[^\d]/g, '').substr(0, 11);
+            }
+        });
+
+        mask.mask(phoneInput);
+        if (phoneInput.value && phoneInput.value.trim() !== '') {
+            let cleaned = phoneInput.value.replace(/[^\d]/g, '');
+            if (cleaned.length === 11 && cleaned.startsWith('8')) {
+                cleaned = '7' + cleaned.substr(1);
+            } else if (cleaned.length === 10) {
+                cleaned = '7' + cleaned;
+            } else if (cleaned.length === 11 && cleaned.startsWith('7')) {
+            } else {
+                cleaned = cleaned.substr(0, 11);
+            }
+            phoneInput.value = '';
+            phoneInput.inputmask.setValue('7' + cleaned.substr(1, 10).padEnd(10, '_'));
+        }
+
+
+        phoneInput.addEventListener('blur', function () {
+            if (this.inputmask && this.inputmask.isComplete() === false) {
+            }
+        });
+    });
+</script>
