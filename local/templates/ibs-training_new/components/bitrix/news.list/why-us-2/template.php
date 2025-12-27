1e5c99f44f989d3cd1023da0ce1b7705b0f02f8d
Kirill <kocherov@addamant.ru>
2025-12-27 22:04:48 +0300

    135524 | Главная / Редизайн блока «Наши отличия от других УЦ»

--- local/templates/ibs-training_new/components/bitrix/news.list/why-us-2/template.php
+++ local/templates/ibs-training_new/components/bitrix/news.list/why-us-2/template.php
@@ -9,7 +9,7 @@ $this->setFrameMode(true);
 ?>
 
 <section class="why-us-section <?=$arParams['CUSTOM_CLASS'];?>">
-    <div class="container">
+    <div class="why-us__container">
 
         <h2 class="why-us__main-title">
             <?php $APPLICATION->IncludeFile(SITE_DIR . 'include/about/program-goals-title.php', [], ['MODE' => 'html']); ?>
@@ -25,11 +25,8 @@ $this->setFrameMode(true);
             <?foreach($arResult["ITEMS"] as $key => $arItem):
                 $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                 $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), ['CONFIRM' => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')]);
-
-                $cardNumber = $key + 1;
-                $isFirst = ($cardNumber === 1);
                 ?>
-                <div class="why-us__card why-us__card--<?= $cardNumber ?>" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                <div class="why-us__card" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                     <?if(!empty($arItem['PROPERTIES']['ICON']['VALUE'])):?>
                         <div class="why-us__icon">
                             <img src="<?=CFile::GetPath($arItem['PROPERTIES']['ICON']['VALUE']);?>" alt="">
