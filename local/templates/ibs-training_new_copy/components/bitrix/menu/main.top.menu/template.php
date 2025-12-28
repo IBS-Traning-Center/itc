dbed2655b474c3bb789bec7586a4719489610869
Redkot1207 <poligraf-pc@mail.ru>
2025-12-28 15:13:25 +0600

    136189 | Общее/Хедер/ При переходе в разделы

--- local/templates/ibs-training_new_copy/components/bitrix/menu/main.top.menu/template.php
+++ local/templates/ibs-training_new_copy/components/bitrix/menu/main.top.menu/template.php
@@ -12,7 +12,7 @@ global $USER;
  * @var $arResult
  * @var $arParams
  * @var string $templateFolder
-*/
+ */
 
 ?>
 <div class="main-top-menu-block">
@@ -21,13 +21,24 @@ global $USER;
             <a href="<?=$value['LINK']?>" class="main-top-menu-item <?= (count($arResult) == $key + 1) ? 'last-menu' : '' ?>">
                 <?php
                 $isCatalog = false;
+                $isPersonal = false;
 
-                if ($value['PARAMS']['CATALOG'] === 'Y') {
+                // Проверка на каталог
+                if (isset($value['PARAMS']['CATALOG']) && $value['PARAMS']['CATALOG'] === 'Y') {
                     $isCatalog = true;
+                }
+
+                // Проверка на личный кабинет
+                if (strpos($value['LINK'], '/personal/') !== false ||
+                    (isset($value['PARAMS']['PERSONAL']) && $value['PARAMS']['PERSONAL'] === 'Y')) {
+                    $isPersonal = true;
                 }
                 ?>
                 <?= ($isCatalog) ? Functions::buildSVG('catalog_icon', $templateFolder . '/images') : ''?>
                 <span class="f-16 <?=($isCatalog) ? 'catalog-link' : ''?>"><?=$value['TEXT']?></span>
+                <?php if ($isPersonal) : ?>
+                    <span class="profile-icon-right"><?= Functions::buildSVG('profile_icon', $templateFolder . '/images') ?></span>
+                <?php endif; ?>
             </a>
         <?php endforeach; ?>
     <?php endif; ?>
