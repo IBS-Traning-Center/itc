8f9bc01474b9daa0bcd1f232e23a93e376c3ddd6
Kirill <kocherov@addamant.ru>
2025-12-29 22:47:30 +0300

    136150 | Главная / Корректировка ЛК в хедере сайта

--- local/templates/ibs-training_new_copy/components/bitrix/menu/main.top.menu/template.php
+++ local/templates/ibs-training_new_copy/components/bitrix/menu/main.top.menu/template.php
@@ -8,38 +8,53 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 
 global $USER;
 
-/**
- * @var $arResult
- * @var $arParams
- * @var string $templateFolder
- */
-
 ?>
 <div class="main-top-menu-block">
     <?php if (!empty($arResult)) : ?>
         <?php foreach ($arResult as $key => $value) : ?>
-            <a href="<?=$value['LINK']?>" class="main-top-menu-item <?= (count($arResult) == $key + 1) ? 'last-menu' : '' ?>">
-                <?php
-                $isCatalog = false;
-                $isPersonal = false;
+            <?php
+            $isCatalog = false;
+            $isPersonal = false;
 
-                // Проверка на каталог
-                if (isset($value['PARAMS']['CATALOG']) && $value['PARAMS']['CATALOG'] === 'Y') {
-                    $isCatalog = true;
-                }
+            if (isset($value['PARAMS']['CATALOG']) && $value['PARAMS']['CATALOG'] === 'Y') {
+                $isCatalog = true;
+            }
 
-                // Проверка на личный кабинет
-                if (strpos($value['LINK'], '/personal/') !== false ||
-                    (isset($value['PARAMS']['PERSONAL']) && $value['PARAMS']['PERSONAL'] === 'Y')) {
-                    $isPersonal = true;
-                }
-                ?>
-                <?= ($isCatalog) ? Functions::buildSVG('catalog_icon', $templateFolder . '/images') : ''?>
-                <span class="f-16 <?=($isCatalog) ? 'catalog-link' : ''?>"><?=$value['TEXT']?></span>
-                <?php if ($isPersonal) : ?>
-                    <span class="profile-icon-right"><?= Functions::buildSVG('profile_icon', $templateFolder . '/images') ?></span>
+            if (strpos($value['LINK'], '/personal/') !== false ||
+                (isset($value['PARAMS']['PERSONAL']) && $value['PARAMS']['PERSONAL'] === 'Y')) {
+                $isPersonal = true;
+            }
+
+            $isLast = (count($arResult) == $key + 1);
+            ?>
+
+            <!-- ВСЕ внутри одного контейнера -->
+            <div class="main-top-menu-item-container <?= $isPersonal ? 'personal-container' : '' ?>">
+                <a href="<?=$value['LINK']?>"
+                   class="main-top-menu-item <?= $isLast ? 'last-menu' : '' ?>">
+
+                    <?= ($isCatalog) ? Functions::buildSVG('catalog_icon', $templateFolder . '/images') : '' ?>
+
+                    <?php if (!$isPersonal || !$USER->IsAuthorized()) : ?>
+                        <span class="f-16 <?= $isCatalog ? 'catalog-link' : '' ?>"><?=$value['TEXT']?></span>
+                    <?php endif; ?>
+
+                    <?php if ($isPersonal) : ?>
+                        <span class="profile-icon-right">
+                            <?= Functions::buildSVG('profile_icon', $templateFolder . '/images') ?>
+                        </span>
+                    <?php endif; ?>
+                </a>
+
+                <?php if ($isPersonal && $USER->IsAuthorized()) : ?>
+                    <!-- Дропдаун теперь внутри того же контейнера -->
+                    <div class="profile-dropdown">
+                        <a href="<?=$value['LINK']?>" class="dropdown-item">Личный кабинет</a>
+                        <a href="?logout=yes&<?=bitrix_sessid_get()?>" class="dropdown-item logout">Выйти</a>
+                    </div>
                 <?php endif; ?>
-            </a>
+            </div>
+
         <?php endforeach; ?>
     <?php endif; ?>
 </div>
