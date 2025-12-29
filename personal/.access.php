618b109a5f79424b0945666991b7af0dca96b15a
Kirill <kocherov@addamant.ru>
2025-12-29 14:22:12 +0300

    136150 | Главная / Корректировка ЛК в хедере сайта

--- personal/.access.php
+++ personal/.access.php
@@ -1,2 +1,8 @@
-<?
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php');
+
+if (!$USER->IsAuthorized()) {
+    LocalRedirect('/auth/');
+    exit();
+}
 ?>
