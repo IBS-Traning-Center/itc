58dcdb8848ce102a91064d329509007fbcf2271d
Redkot1207 <poligraf-pc@mail.ru>
2025-12-29 17:55:41 +0600

    добавление пунктов меню в мобильное меню

--- local/templates/ibs-training_new/components/bitrix/menu/hidden.bottom.mobile/template.php
+++ local/templates/ibs-training_new/components/bitrix/menu/hidden.bottom.mobile/template.php
@@ -10,11 +10,15 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
  * @var string $templateFolder
 */
 
+global $USER;
 ?>
 <?php if (!empty($arResult)) : ?>
     <div class="hidden-bottom-mobile-menu-block">
         <div class="container hidden-bottom-mobile-menu-container">
-            <?php foreach ($arResult as $key => $value) : ?>
+            <?php foreach ($arResult as $key => $value) : 
+                if($value['PARAMS']['HIDE_AUTH'] == 'Y' && !$USER->IsAuthorized()){
+                    continue;
+                }?>
                 <a href="<?= $value['LINK'] ?>" class="hidden-bottom-mobile-menu-item">
                     <span class="f-20"><?= $value['TEXT'] ?></span>
                 </a>
