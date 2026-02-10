5644118dea44c206ea718a8bddd30a668dd9a694
Kirill <kocherov@addamant.ru>
2026-02-10 21:16:07 +0300

    136582 | ЛК/Настройки/ Пароль/ верстка не по макету

--- local/templates/ibs-training_new/components/bitrix/system.auth.changepasswd/.default/script.js
+++
@@ -1,8 +0,0 @@
-function togglePassword(icon) {
-    const input = icon.previousElementSibling;
-    if (input.type === 'password') {
-        input.type = 'text';
-    } else {
-        input.type = 'password';
-    }
-}
